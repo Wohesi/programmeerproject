2a5cd5098564e74c75525e2d9b1a88780a538cae
Wout <woutsingerling@gmail.com>
2018-01-15 22:28:48 +0100

    XML parser -- current error: I/System.out: com.android.volley.TimeoutError

--- app/src/main/java/com/example/gebruiker/boardgameapp/SearchFragment.java
+++ app/src/main/java/com/example/gebruiker/boardgameapp/SearchFragment.java
@@ -1,15 +1,21 @@
 package com.example.gebruiker.boardgameapp;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -25,6 +31,7 @@ import java.io.InputStream;
 import java.io.StringReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -34,14 +41,16 @@ import java.util.ArrayList;
 
 public class SearchFragment extends Fragment {
 
-    private String url;
 
     private RequestQueue requestQueue;
-    private XmlPullParserFactory factory;
+    private bg_searchtile searchtile;
+    private ArrayList<bg_searchtile> searchTiles = new ArrayList<>();
+
     private XmlPullParser xpp;
 
-    private bg_searchtile bg_searchtile;
-    private ArrayList<bg_searchtile> tiles = new ArrayList<>();
+    private String url;
+    private static final String ns = null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,18 +58,14 @@ public class SearchFragment extends Fragment {
 
         View searchFragment =  inflater.inflate(R.layout.fragment_search, container, false);
 
-
-        // getting xml data
         requestQueue = Volley.newRequestQueue(getContext());
 
         try {
-            factory = XmlPullParserFactory.newInstance();
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             xpp = factory.newPullParser();
-        } catch (XmlPullParserException e ) {
+        } catch (XmlPullParserException e) {
             e.printStackTrace();
         }
-
-
 
         // Searching
         SearchView searchView = (SearchView) searchFragment.findViewById(R.id.search_bar);
@@ -73,26 +78,28 @@ public class SearchFragment extends Fragment {
                         s  = s.replace(" ", "%20");
 
                         // getting the correct url of searched game.
-                        url = "http://www.boardgamegeek.com/xmlapi/search?search="+s+"&exact=1";
-                        System.out.println(url);
+                        url = "https://www.boardgamegeek.com/xmlapi/search?search="+s+"&exact=1";
+
 
                         final StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
+
                                 try {
                                     xpp.setInput(new StringReader(response));
                                     int event = xpp.getEventType();
-                                    String tag="", value="";
+
+                                    String tag = "", value = "";
 
                                     while(event != XmlPullParser.END_DOCUMENT) {
-
                                         tag = xpp.getName();
-
-                                        switch (event) {
+                                        switch(event) {
                                             case XmlPullParser.START_TAG:
                                                 if(tag.equals("boardgame")) {
-                                                    bg_searchtile = new bg_searchtile();
-                                                    tiles.add(bg_searchtile);
+                                                    searchtile = new bg_searchtile();
+
+                                                    // not working??
+                                                    searchTiles.add(searchtile);
                                                 }
                                                 break;
                                             case XmlPullParser.TEXT:
@@ -101,38 +108,37 @@ public class SearchFragment extends Fragment {
                                             case XmlPullParser.END_TAG:
 
                                                 switch (tag) {
-                                                    case "objectid":
-                                                        bg_searchtile.getID(Integer.parseInt(value));
-                                                        break;
                                                     case "name":
-                                                        bg_searchtile.getName(value);
+                                                        searchtile.setName(value);
                                                         break;
                                                     case "yearpublished":
-                                                        bg_searchtile.getYearpublished(Integer.parseInt(value));
-                                                        break;
+                                                        searchtile.setYearpublished(Integer.parseInt(value));
                                                 }
                                                 break;
                                         }
                                         event = xpp.next();
                                     }
 
-                                } catch (XmlPullParserException | IOException e) {
+
+                                } catch (XmlPullParserException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
                                     e.printStackTrace();
                                 }
-
-                                for (int i = 0; i < tiles.size(); i++) {
-                                    System.out.println(tiles.get(i));
+                                for(int i = 0; i < searchTiles.size(); i++ ) {
+                                    searchTiles.get(i).print_info();
                                 }
 
                             }
                         }, new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
-
+                                System.out.println(error);
                             }
                         });
 
                         requestQueue.add(stringRequest);
+
                         return false;
                     }
 
@@ -145,12 +151,9 @@ public class SearchFragment extends Fragment {
                 }
         );
 
-
         // Inflate the layout for this fragment
         return searchFragment;
     }
-
-
 
 }
 
