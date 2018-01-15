bd848893e638d35e9bb3d41b61ea279c2d0017c6
Wout <woutsingerling@gmail.com>
2018-01-15 13:50:09 +0100

    added searchview functionality

--- app/src/main/java/com/example/gebruiker/boardgameapp/SearchFragment.java
+++ app/src/main/java/com/example/gebruiker/boardgameapp/SearchFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 
 /**
@@ -20,8 +21,34 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View searchFragment =  inflater.inflate(R.layout.fragment_search, container, false);
+
+        // Searching
+        SearchView searchView = (SearchView) searchFragment.findViewById(R.id.search_bar);
+        searchView.setOnQueryTextListener(
+                new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String s) {
+                        // if text has been changed, or filtering apply here
+                        System.out.println("This has been submitted: "+ s);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String s) {
+                        // perform the search query
+                        System.out.println("this has been changed:  "+ s);
+                        return false;
+                    }
+                }
+        );
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return searchFragment;
     }
 
 
