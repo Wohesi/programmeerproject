54f68d30c109c34c83172e71953d40c947efba93
Wout <woutsingerling@gmail.com>
2018-01-18 17:25:00 +0100

    added basic structure for large boardgame fragment as well as the calendar

--- app/src/main/java/com/example/gebruiker/boardgameapp/LargeBgFragment.java
+++ app/src/main/java/com/example/gebruiker/boardgameapp/LargeBgFragment.java
@@ -7,14 +7,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.w3c.dom.Text;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LargeBgFragment extends Fragment {
 
+    // values for visuals
     private Button openCalendar;
+    private TextView title, year, length_setter, weight_setter, avgRating_setter, numPlayers_setter;
+    private ImageView background_img;
+
+
+    // values for API connection
+    private String url;
+    private RequestQueue requestQueue;
+    private XmlPullParser xpp;
+    private String id;
+
 
     public LargeBgFragment() {
         // Required empty public constructor
@@ -26,8 +51,76 @@ public class LargeBgFragment extends Fragment {
                              Bundle savedInstanceState) {
         View LargeBgFragment = inflater.inflate(R.layout.bg_large, container, false);
 
-        openCalendar = LargeBgFragment.findViewById(R.id.openCalendar);
+        title = LargeBgFragment.findViewById(R.id.title);
+        year = LargeBgFragment.findViewById(R.id.year);
+        length_setter = LargeBgFragment.findViewById(R.id.length_setter);
+        weight_setter = LargeBgFragment.findViewById(R.id.weight_setter);
+        avgRating_setter = LargeBgFragment.findViewById(R.id.avg_rating_setter);
+        numPlayers_setter = LargeBgFragment.findViewById(R.id.num_players_setter);
+        background_img = LargeBgFragment.findViewById(R.id.background_img);
 
+
+        // get the calendar dialog
+        openCalendar = LargeBgFragment.findViewById(R.id.openCalendar);
+        makeEvent();
+
+        requestQueue = Volley.newRequestQueue(getContext());
+
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            xpp = factory.newPullParser();
+        } catch (XmlPullParserException e ) {
+            e.printStackTrace();
+        }
+
+        loadData();
+        return LargeBgFragment;
+    }
+
+    public void loadData() {
+        final StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    xpp.setInput(new StringReader(response));
+                    int event = xpp.getEventType();
+
+                    String tag = "", value = "";
+
+                    while(event != XmlPullParser.END_DOCUMENT) {
+                        tag = xpp.getName();
+
+                        switch (event) {
+                            case XmlPullParser.START_DOCUMENT:
+                                if(tag.equals(""))
+                                break;
+                            case XmlPullParser.TEXT:
+                                value = xpp.getText();
+
+                            case XmlPullParser.END_TAG:
+
+                                switch (tag) {
+                                    case "":
+                                        break;
+                                    case "":
+                                        break;
+                                    // more depending on api respnse
+                                }
+                        }
+
+                    }
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                } catch (IOException e ) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        requestQueue.add(stringRequest);
+    }
+
+    public void makeEvent() {
         openCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,11 +129,8 @@ public class LargeBgFragment extends Fragment {
                 dialog.show(getFragmentManager(), "CalendarDialog");
 
             }
+
         });
-
-
-
-        return LargeBgFragment;
     }
 
 }
