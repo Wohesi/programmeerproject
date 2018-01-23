872587abdeb6b7e7939a5cddc31fd58538bca243
Wohesi <woutsingerling@gmail.com>
2018-01-23 10:48:15 +0100

    added signout button

--- app/src/main/java/com/example/gebruiker/boardgameapp/UserFragment.java
+++ app/src/main/java/com/example/gebruiker/boardgameapp/UserFragment.java
@@ -6,6 +6,9 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -13,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class UserFragment extends Fragment {
 
+    private FirebaseAuth mAuth;
+    public Button logout;
 
     public UserFragment() {
         // Required empty public constructor
@@ -23,7 +28,20 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logout = view.findViewById(R.id.sign_out);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+            }
+        });
+
+        return view;
     }
+
 
 }
